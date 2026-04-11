184e00a6bdb57b218205e5b704919730d070d9e4
cja2001 <92105486+cja2001@users.noreply.github.com>
2026-04-11 15:12:51 -0600

    login

--- login.js
+++ login.js
@@ -80,7 +80,7 @@ document.addEventListener('DOMContentLoaded', () => {
       }
 
       showGlobalAlert('✓ Acceso concedido. Redirigiendo...', 'success');
-      setTimeout(() => { window.location.href = 'menu/menu.html'; }, 100);s
+      setTimeout(() => { window.location.href = 'menu/menu.html'; }, 10);s
 
     } catch (err) {
       showGlobalAlert('Error de conexión. Intenta de nuevo.');
